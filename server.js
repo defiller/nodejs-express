88e719b9437de8958ec3224028ccae539bf47edd
patrickcunha <patrick.cunha336@gmail.com>
2018-11-19 19:16:14 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-const app = require('express')()
+const express = require('express')
+const nunjucks = require('nunjucks')
+const bodyparser = require('body-parser')
+const app = express()
 
-app.listen(3000)
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+})
+
+app.use(express.json())
+app.use(bodyparser())
+
+app.set('view engine', 'njk')
+
+//routes
+app.get("/", (req, res) => {})
+app.get("/check", (req, res) => {})
+app.get("/major", (req, res) => {})
+app.get("/minor", (req, res) => {})
+
+app.listen(3000, () => console.log("server started"))
