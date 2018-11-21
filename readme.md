92e982122096bed0de45947e57696eb49ba37b41
patrickcunha <patrick.cunha336@gmail.com>
2018-11-20 21:43:30 -0300

    update babel es6 syntax

---
+++ readme.md
@@ -0,0 +1,21 @@
+#Readme with aditional descriptions
+
+##Tecnology
+
+### Javascript
+
+The application was build with javascript using the most recents updates of the language following the documentation
+of ecmascript 2018, using of nodejs / express to construct middleware and routes of the application
+
+### Nunjucks
+
+I use nunjucks as the view engine of this application
+
+## Objective
+
+This application was build as a experiment / training
+
+## What it does?
+
+The application expects an age input and verify if the data given is less or greater then 18 and deal with the result
+using middlewares and routes to render the feedback on a template nunjucks
--- server.js
+++ server.js
@@ -1,23 +1,49 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true
+  autoescape: true,
+  express: app,
+  watch: true
 })
 
-app.use(express.json())
-app.use(bodyparser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.set('view engine', 'njk')
 
-//routes
-app.get("/", (req, res) => {})
-app.get("/check", (req, res) => {})
-app.get("/major", (req, res) => {})
-app.get("/minor", (req, res) => {})
+// routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.post('/check', (req, res) => {
+  const {
+    age
+  } = req.body
 
-app.listen(3000, () => console.log("server started"))
+  if (age >= 18) res.redirect(`/major/${age}`)
+  else res.redirect(`/minor/${age}`)
+})
+
+app.get('/major', (req, res) => res.redirect('/'))
+app.get('/minor', (req, res) => res.redirect('/'))
+
+app.get('/major/:age', (req, res) => {
+  res.render('age', {
+    majority: 'maior',
+    age: req.params.age
+  })
+})
+
+app.get('/minor/:age', (req, res) => {
+  res.render('age', {
+    majority: 'menor',
+    age: req.params.age
+  })
+})
+
+app.listen(3000, () => console.log(`server started on url http://localhost:${3000}`))
